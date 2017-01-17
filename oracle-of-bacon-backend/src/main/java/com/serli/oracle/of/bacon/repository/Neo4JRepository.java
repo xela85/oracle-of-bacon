f5ce8cd1f5ed305c5b002f76d09998d2e09fa644
Nat-Faeeria <nathanael.gimenez@serli.com>
2017-01-17 14:36:30 +0100

    Implemented the Neo4j part

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/Neo4JRepository.java
@@ -1,26 +1,54 @@
 package com.serli.oracle.of.bacon.repository;
 
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Path;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.neo4j.driver.v1.Values.parameters;
 
 
 public class Neo4JRepository {
     private final Driver driver;
 
     public Neo4JRepository() {
-        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"));
+        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "workshop"));
     }
 
     public List<?> getConnectionsToKevinBacon(String actorName) {
         Session session = driver.session();
 
-        // TODO implement Oracle of Bacon
-        return null;
+        StatementResult query = session.run("MATCH r = shortestPath(" +
+                "(kevin:Actor {name:'Bacon, Kevin (I)'})" +
+                "-[*]-" +
+                "(parametrizedActor:Actor {name:{name}}))" +
+                " RETURN r;",
+                parameters("name", actorName));
+
+        List<GraphItem> result = new ArrayList<>();
+
+        while (query.hasNext()) {
+            Record record = query.next();
+            Path path = record.get("r").asPath();
+            path.nodes().forEach(x -> {
+                result.add(new GraphNode(x.id(),
+                        x.asMap().values().iterator().next().toString(),
+                        x.labels().iterator().next()));
+            });
+            path.relationships().forEach(x -> {
+                result.add(
+                    new GraphEdge(
+                        x.id(),
+                        x.startNodeId(),
+                        x.endNodeId(),
+                        x.type()
+                    )
+                );
+            });
+        }
+        return result;
     }
 
     private static abstract class GraphItem {
@@ -55,6 +83,17 @@ public class Neo4JRepository {
             this.value = value;
             this.type = type;
         }
+
+        @Override
+        public String toString() {
+            return String.format(
+                "{\"data\":" +
+                    "{\"id\": \"%s\"," +
+                    "\"type\":\"%s\"," +
+                    "\"value\": \"%s\"}" +
+                "}",
+                id, type,value);
+        }
     }
 
     private static class GraphEdge extends GraphItem {
@@ -67,6 +106,18 @@ public class Neo4JRepository {
             this.source = source;
             this.target = target;
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.format(
+                    "{\"data\":{ " +
+                        "\"id\": \"%s\"," +
+                        "\"source\":\"%s\"," +
+                        "\"target\":\"%s\"," +
+                        "\"value\": \"%s\"}" +
+                    "}",
+                    id, source, target , value);
         }
     }
 }
