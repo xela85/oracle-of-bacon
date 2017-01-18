2a3f917216726c3c21e909548febbe82eeafba7d
Nat-Faeeria <nathanael.gimenez@serli.com>
2017-01-18 10:44:59 +0100

    final Neo4J tests

--- oracle-of-bacon-backend/src/test/java/com/serli/oracle/of/bacon/repository/Neo4JRepositoryTest.java
+++ oracle-of-bacon-backend/src/test/java/com/serli/oracle/of/bacon/repository/Neo4JRepositoryTest.java
@@ -12,15 +12,6 @@ import static org.junit.Assert.*;
  * Created by ferrilata on 17/01/17.
  */
 public class Neo4JRepositoryTest {
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
 
     @Test
     public void getConnectionsToKevinBacon() throws Exception {
